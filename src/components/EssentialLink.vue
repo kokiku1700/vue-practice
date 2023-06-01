820258fc328fca36873a831107c8b9af82d51817
kokiku1700 <kokiku1700@naver.com>
2023-06-01 22:42:20 +0900

    2023/06/01

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,13 +1,7 @@
 <template>
-  <q-item
-    clickable
-    tag="a"
-    :href="link"
-  >
-    <q-item-section
-      v-if="icon"
-      avatar
-    >
+  <!--target="_blank" tag="a" :href="link"-->
+  <q-item clickable :to="link">
+    <q-item-section v-if="icon" avatar>
       <q-icon :name="icon" />
     </q-item-section>
 
@@ -19,30 +13,30 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'EssentialLink',
+  name: "EssentialLink",
   props: {
     title: {
       type: String,
-      required: true
+      required: true,
     },
 
     caption: {
       type: String,
-      default: ''
+      default: "",
     },
 
     link: {
       type: String,
-      default: '#'
+      default: "/",
     },
 
     icon: {
       type: String,
-      default: ''
-    }
-  }
-})
+      default: "",
+    },
+  },
+});
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -8,12 +8,20 @@ const routes = [
       { path: '/vue', component: () => import('pages/VueEx.vue') },
       { path: '/shop', component: () => import('pages/ShoppingList.vue') },
       { path: '/todo', component: () => import('pages/Todo.vue') },
-      { path: '/login', component: () => import('src/pages/Login.vue')},
       { path: "/dbtodo", component: () => import("pages/DbTodo.vue") },
       { path: "/storetodo", component: () => import("pages/StoreTodo.vue") },
     ]
   },
 
+  {
+    path: "/signin",
+    component: () => import("layouts/SignIn.vue"),
+  },
+
+  {
+    path: "/login",
+    component: () => import("layouts/Login.vue"),
+  },
   
   {
     path: '/:catchAll(.*)*',
